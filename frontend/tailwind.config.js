94fda6a029ce3d22aadb9d1f4678837fae4d584d
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-08-22 11:13:26 -0400

    initialized packages and configs

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,tsx,ts}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+	content: ["./src/**/*.{html,tsx,ts}"],
+	theme: {
+		extend: {
+			colors: {
+				light: "#FFC7F9",
+				medium: "#79468A",
+				dark: "#140428",
+				glow: "#DC78FF",
+			},
+			animation: {
+				"infinite-scroll": "infinite-scroll 7s linear infinite",
+			},
+			keyframes: {
+				"infinite-scroll": {
+					from: { transform: "translateX(0)" },
+					to: { transform: "translateX(100%)" },
+				},
+			},
+		},
+	},
+	plugins: [],
 };
