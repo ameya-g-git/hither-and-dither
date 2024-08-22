25e07c0b84e2860f366850e7050ee8dc59e5ccc5
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-08-22 11:13:34 -0400

    working on hero page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,29 @@
+import wave from "./assets/pixel_doodles/wave.svg";
+import ScrollingImage from "./components/ScrollingImage";
+
 export default function App() {
-  return <h1 className="font-bold">Ah</h1>;
+	const waveElements = (n: number) => {
+		return Array(n)
+			.fill("")
+			.map((_, i) => (
+				<ScrollingImage ltr={false} width="w-10">
+					<img key={i} className="w-12 -mr-2" src={wave} />
+				</ScrollingImage>
+			));
+	};
+
+	return (
+		<>
+			<div className="flex items-center w-screen h-screen">
+				<div id="hero-text">
+					<h1 className="">
+						hither <br /> & dither
+					</h1>
+					<div className="flex flex-row w-full *:-mr-1.5">
+						{waveElements(1)}
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
---
+++ frontend/src/components/ScrollingImage.tsx
@@ -0,0 +1,28 @@
+import clsx from "clsx";
+import { ReactNode } from "react";
+interface props {
+	ltr: boolean;
+	width: string;
+	children: ReactNode;
+}
+
+export default function ScrollingImage({ ltr, width, children }: props) {
+	const containerStyles = clsx(
+		`flex items-center h-full ${width} overflow-hidden ${
+			ltr ? "" : "-scale-x-100"
+		}`
+	);
+
+	const imageStyles = clsx(
+		`flex flex-row last:m-0 justify-end animate-infinite-scroll`
+	);
+
+	return (
+		<div role="presentation" className={containerStyles}>
+			<div className={imageStyles}>
+				{children}
+				{children}
+			</div>
+		</div>
+	);
+}
