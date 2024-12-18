d5bd1ba9727b2ca0e5e4567323cdf76632c87e0c
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-12-18 13:34:08 -0500

    changed structure of window image to work with canvas

--- frontend/src/components/WindowImage.tsx
+++ frontend/src/components/WindowImage.tsx
@@ -1,15 +1,16 @@
 import icons from "../assets/pixel_doodles/icons.svg";
 import shadow from "../assets/img/bayershadow.webp";
+import { ReactNode } from "react";
 
 interface WindowImageProps {
 	x?: number;
 	y?: number;
 	className: string;
-	img: string;
 	title: string;
+	children: ReactNode;
 }
 
-export default function WindowImage({ x, y, className, img, title }: WindowImageProps) {
+export default function WindowImage({ x, y, className, title, children }: WindowImageProps) {
 	return (
 		<div
 			className={`${className} absolute flex justify-center`}
@@ -23,10 +24,10 @@ export default function WindowImage({ x, y, className, img, title }: WindowImage
 			</div>
 			<div className="flex flex-col w-full h-full gap-2 px-3 py-2 border-[6px] text-dark bg-medium border-dark overflow-clip">
 				<div className="flex flex-row items-center justify-between w-full">
-					<h3 className="pt-3">{title}</h3>
+					<h3 className="pt-3">{title.length > 12 ? title.slice(0, 12) + "..." : title}</h3>
 					<img className="h-8" src={icons} alt="" />
 				</div>
-				<img className="object-cover h-full overflow-hidden border-4 border-box border-dark" src={img} alt="" />
+				{children}
 			</div>
 		</div>
 	);
