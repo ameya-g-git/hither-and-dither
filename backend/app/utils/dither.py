052a00b9935e181564520b494906424be4a529ee
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2025-04-18 19:55:52 -0400

    implemented euclidean_dist and RGB dithering

--- backend/app/utils/dither.py
+++ backend/app/utils/dither.py
@@ -8,16 +8,23 @@ from PIL import Image
 #    idx = np.argmin(np.sum((old_val[None,:] - p)**2, axis=1))
 #    return p[idx]
 
+
 # do some more reading on how to do FS dithering with RGB just so i can adjust this implementation appropriately
 # i mean i figure its just   find the closest colour based on euclidean distance. the most effective means of that is unknown but ! we shall see
 # TODO: also  fetching b64 encoded images from the server  decoding them   and zipping them with JSZip or whatever
+def euclidean_dist(col1: list[int], col2: list[int]):
+    dR = col1[0] - col2[0]
+    dG = col1[1] - col2[1]
+    dB = col1[2] - col2[2]
+
+    return (0.3 * dR**2) + (0.59 * dG**2) + (0.11 * dB**2)
 
 
 def dither_general(img: Image, img_size: int, scale: int, weights: list[list[float]], palette: list[str]):
     width, height = img.size
     weight_matrix = np.array(weights)
 
-    img = img.convert("L")
+    # img = img.convert("L")
 
     weight_h, weight_w = weight_matrix.shape
     weight_center = weight_w // 2
@@ -29,23 +36,20 @@ def dither_general(img: Image, img_size: int, scale: int, weights: list[list[flo
         img_width = round((img_size / height) * width)
         img_height = img_size
 
-    fwd_arr = np.zeros(img_width)
-    fwd_arr2 = np.zeros(img_width)
+    fwd_arr = np.zeros((img_width, 3))
+    fwd_arr2 = np.zeros((img_width, 3))
 
     img_resize = img.resize((img_width, img_height), Image.Resampling.LANCZOS)
-    img_arr = np.array(img_resize, dtype=float) / 255
-
-    # for now, im just doing black and white schtuff
+    img_arr = np.array(img_resize, dtype=float)
+    print(palette)
 
     for ir in range(img_height):
         for ic in range(img_width):
             old_val = img_arr[ir, ic].copy()
-            new_val = (
-                round(old_val) if old_val < 1 else 1
-            )  # TODO: this is where the replacement for the palette thing comes in
+            new_val = min(palette, key=lambda c: euclidean_dist(old_val, c))
 
             img_arr[ir, ic] = new_val
-            err = old_val - new_val  # TODO: and as such, the error format will change
+            err = np.clip(np.subtract(old_val, new_val), -255, 255)
 
             for row in range(weight_h):
                 for col in range(weight_w):
@@ -54,26 +58,34 @@ def dither_general(img: Image, img_size: int, scale: int, weights: list[list[flo
                     else:
                         if ic + col - weight_center < img_width:
                             if row == 0:
-                                img_arr[ir, ic + col - weight_center] += err * weight_matrix[row, col]
+                                np.add(
+                                    img_arr[ir, ic + col - weight_center],
+                                    err * weight_matrix[row, col],
+                                    out=img_arr[ir, ic + col - weight_center],
+                                )
                             elif row == 1:
-                                fwd_arr[ic + col - weight_center] += err * weight_matrix[row, col]
-
+                                np.add(
+                                    fwd_arr[ic + col - weight_center],
+                                    err * weight_matrix[row, col],
+                                    out=fwd_arr[ic + col - weight_center],
+                                )
                             else:
-                                fwd_arr2[ic + col - weight_center] += err * weight_matrix[row, col]
+                                np.add(
+                                    fwd_arr2[ic + col - weight_center],
+                                    err * weight_matrix[row, col],
+                                    out=fwd_arr2[ic + col - weight_center],
+                                )
         if ir < (img_height - 1):
-            img_arr[ir + 1] += fwd_arr
+            np.add(img_arr[ir + 1], fwd_arr, out=img_arr[ir + 1])
         if ir < (img_height - 2):
-            img_arr[ir + 2] += fwd_arr2
-        fwd_arr = np.zeros(img_width)
-        fwd_arr2 = np.zeros(img_width)
+            np.add(img_arr[ir + 2], fwd_arr2, out=img_arr[ir + 2])
+        fwd_arr = np.zeros((img_width, 3))
+        fwd_arr2 = np.zeros((img_width, 3))
 
-    img_arr = np.clip(img_arr, 0, 1)
-    carr = np.array(img_arr * 255, dtype=np.uint8)
+    carr = np.array(img_arr, dtype=np.uint8)
     dithered_image = Image.fromarray(carr)
     dithered_image_resize = dithered_image.resize((img_width * scale, img_height * scale), Image.Resampling.NEAREST)
     return dithered_image_resize
-
-    # TODO: okay im done for today Ough   i guess i should test this out next   receive the image from this function and display it in a modal maybe
 
 
 # def palette_reduce(img, nc):
---
+++ backend/app/utils/hex_to_array.py
@@ -0,0 +1,18 @@
+def hex_to_dec(hex_char: str):
+    if hex_char.isdigit():
+        return int(hex_char)
+    return ord(hex_char) - ord("a") + 10
+
+
+def hex_to_array(hex: str):
+    R = hex[1:3]
+    G = hex[3:5]
+    B = hex[5:7]
+
+    # #140428
+
+    R_val = hex_to_dec(R[1]) + (hex_to_dec(R[0]) * 16)
+    G_val = hex_to_dec(G[1]) + (hex_to_dec(G[0]) * 16)
+    B_val = hex_to_dec(B[1]) + (hex_to_dec(B[0]) * 16)
+
+    return [R_val, G_val, B_val]
